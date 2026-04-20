a81bb03ad43ae3dc2d30c16690d5cd7d45e9c580
RicoDalB <riccardodalbianco02@gmail.com>
2026-04-20 18:33:09 +0200

    trained model

---
+++ build/CMakeFiles/backend.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backend.dir/api/api_server.cpp.o"
+  "CMakeFiles/backend.dir/api/api_server.cpp.o.d"
+  "CMakeFiles/backend.dir/app/predictor.cpp.o"
+  "CMakeFiles/backend.dir/app/predictor.cpp.o.d"
+  "CMakeFiles/backend.dir/app/trainer.cpp.o"
+  "CMakeFiles/backend.dir/app/trainer.cpp.o.d"
+  "CMakeFiles/backend.dir/io/dataset_utils.cpp.o"
+  "CMakeFiles/backend.dir/io/dataset_utils.cpp.o.d"
+  "CMakeFiles/backend.dir/io/mnist_reader.cpp.o"
+  "CMakeFiles/backend.dir/io/mnist_reader.cpp.o.d"
+  "CMakeFiles/backend.dir/io/weights_io.cpp.o"
+  "CMakeFiles/backend.dir/io/weights_io.cpp.o.d"
+  "CMakeFiles/backend.dir/main.cpp.o"
+  "CMakeFiles/backend.dir/main.cpp.o.d"
+  "CMakeFiles/backend.dir/math/matrix.cpp.o"
+  "CMakeFiles/backend.dir/math/matrix.cpp.o.d"
+  "CMakeFiles/backend.dir/nn/activations.cpp.o"
+  "CMakeFiles/backend.dir/nn/activations.cpp.o.d"
+  "CMakeFiles/backend.dir/nn/layer.cpp.o"
+  "CMakeFiles/backend.dir/nn/layer.cpp.o.d"
+  "CMakeFiles/backend.dir/nn/loss.cpp.o"
+  "CMakeFiles/backend.dir/nn/loss.cpp.o.d"
+  "CMakeFiles/backend.dir/nn/neural_network.cpp.o"
+  "CMakeFiles/backend.dir/nn/neural_network.cpp.o.d"
+  "backend"
+  "backend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
